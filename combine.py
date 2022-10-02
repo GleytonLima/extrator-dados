28ac04cdc5d5f67313db12cf2406d30368e276b2
Gleyton Lima <gleytonclima@gmail.com>
2022-10-01 21:54:05 -0300

    Adicionando script para combinar arquivos csv

---
+++ combine.py
@@ -0,0 +1,15 @@
+import glob
+import os
+
+import pandas as pd
+
+os.chdir("./data")
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+# combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+# export to csv
+combined_csv.to_csv("sia_sus_manaus.csv",
+                    index=False,
+                    encoding='utf-8-sig')
